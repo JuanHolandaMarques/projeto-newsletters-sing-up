ef70da73a903887177dabedd64279bf3e2bd19e7
Juan Holanda <123557653+JuanHolandaMarques@users.noreply.github.com>
2023-08-30 11:32:15 -0300

    preparando a base de script do projeto
    base do codigo em js, para estilizar corretamente depois de ter terminado o CSS da pagina.

---
+++ js/script.js
@@ -0,0 +1,33 @@
+const form = document.querySelector('#form')
+const emailInput = document.querySelector('#email')
+const buttonInput = document.querySelector('#button')
+
+form.addEventListener('submit', (event) => {
+  event.preventDefault()
+
+  //verificar se o campo de email ta vazio ou mal digitado
+  if (emailInput.value === "" || !isEmailValid(emailInput.value)) {
+    alert("Por favor, preencha o seu email corretamente")
+    return
+  }else{
+    window.location.href = 'page-login.html'
+  }
+})
+
+  //verificando se o email está minimamente correto.
+function isEmailValid(email) {
+  const emailRegex = new RegExp(
+      /^[a-zA-Z0-9.-_]+@[a-zA-Z0-9.-_]+\.[a-zA-Z]{2,}$/
+  )
+
+  if(emailRegex.test(email)){
+    return true
+  }else{
+    return false
+  }
+}
+
+
+
+
+

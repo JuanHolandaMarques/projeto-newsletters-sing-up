ee9e3124f643f2ac251247f1d0a1bea180d55fc2
Juan Holanda <123557653+JuanHolandaMarques@users.noreply.github.com>
2023-10-27 17:13:09 -0300

    adicionando link de retornar para o index

--- js/scriptP2.js
+++ js/scriptP2.js
@@ -1,7 +1,7 @@
 const inputText = document.getElementById('Email')
 const button = document.querySelector('button')
 
+//mostrando o email que o usuario cadastrou
 const userEmail = localStorage.getItem("emailData")
-
 
 inputText.value = userEmail
